b4808e5450eba971837c3a47c1a251519d43bf12
diego <diego@DESKTOP-3V34RD5>
2026-09-18 12:58:53 -0500

    Add services layer, DTOs, AutoMapper and match board (Module 4)

--- GolBet.Web/Program.cs
+++ GolBet.Web/Program.cs
@@ -1,6 +1,9 @@
 using GolBet.Repositories.Data;
 using GolBet.Repositories.Implementations;
 using GolBet.Repositories.Interfaces;
+using GolBet.Services.Implementations;
+using GolBet.Services.Interfaces;
+using GolBet.Services.Mapping;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,34 +12,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 //Este es el nuevo código  
-
 builder.Services.AddDbContext<AppDbContext>(options =>
 
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+  options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 // Open generic registration: one line, a repository for every entity 
-
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
 
 
 // Specific repositories 
-
 builder.Services.AddScoped<IMatchRepository, MatchRepository>();
+
+// AutoMapper: scans the assembly containing MappingProfile for all profiles 
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
+
+// Business services 
+builder.Services.AddScoped<IMatchService, MatchService>();
 
 var app = builder.Build();
 
 // Seed the database on startup 
-
 using (var scope = app.Services.CreateScope())
-
 {
-
     var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
     await DbSeeder.SeedAsync(context);
-
 }
 
 // Configure the HTTP request pipeline.
@@ -49,13 +52,9 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
